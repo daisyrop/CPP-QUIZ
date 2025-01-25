00f55efbce4908d062e4f51c5d3cb60963949ef3
daisyrop <ropdaisy@zetech.ac.ke>
2025-01-25 21:43:38 +0300

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <cmath> 
+using namespace std;
+
+double calculatecylindervolume(double radius, double height) {
+	double volume= M_PI*radius*radius*height;
+	return volume;
+}
+ int main(){
+ 	double radius, height;
+ 	cout<<"enter the radius of the cylinder:";
+ 	cin>>radius;
+ 	
+ 	cout<<"enter the height of the cylinder:";
+ 	cin>>height;
+ 	
+ 	double volume= calculatecylindervolume(radius, height);
+ 	cout<<"The volume of the cylinder is:"<<volume<<endl;
+ 	
+ 	return 0;
+ 	
+ }
